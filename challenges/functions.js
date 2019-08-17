98bd49866b6b3141ee3cf86a8faa9175387e11c3
Veto Ramirez <vetorobert3@gmail.com>
2019-08-17 10:07:53 -0700

    Started om callbacks.

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,16 +1,19 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a higher-order function that accepts a callback
   * Create a higher-order function named consume that can take 3 parameters.
   * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, cb) {
+  return cb(arg1, arg2);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
@@ -21,11 +24,11 @@
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
-// ==== Closures ==== 
+// ==== Closures ====
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:
 
 
 const external = "I'm outside the function";
